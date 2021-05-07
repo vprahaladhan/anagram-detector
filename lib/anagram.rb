b351d7029567174566ee273d3ecb258b27572c1e
prahal <vprahaladhan@gmail.com>
2021-05-07 10:58:15 +1000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word.strip
+  end
+
+  attr_accessor :word
+
+  def match(array_of_words)
+    anagrams = []
+    array_of_words.each do |word|
+      anagram = true
+      if (word.strip.size == @word.size) then 
+        @word.split("").uniq.each do |char|
+          if (@word.count(char) != word.count(char)) then
+            anagram = false
+          end
+        end
+      else anagram = false
+      end
+      anagram ? anagrams << word : ''  
+    end
+    anagrams
+  end  
+end
